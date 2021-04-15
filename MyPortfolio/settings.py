4f89d49e71bb14306dc2cea320efc3adb6ac140d
ABHI <am@MacBook-Pro.local>
2021-04-15 06:17:58 -0700

    adds blog app

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR,"blog/templates/blog")
+STATIC_DIR = os.path.join(BASE_DIR,"blog/static")
+MEDIA_DIR = os.path.join(BASE_DIR,"media")
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'blog',
 ]
 
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'MyPortfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +81,8 @@ WSGI_APPLICATION = 'MyPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myWeb',
     }
 }
 
@@ -119,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    STATIC_DIR,
+]
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from blog.models import Post,Comment
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CommentForm,PostForm,ContactForm
+from django.urls import reverse_lazy
 # Create your views here.
+from django.views.generic import (ListView,DetailView,TemplateView,
+                                  CreateView,DeleteView,UpdateView)
+
+class Thankyoupage(TemplateView):
+    template_name = 'blog/Thank.html'
+
+class Thankcommentpage(TemplateView):
+    template_name = 'blog/Thankcomment.html'
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_draft_list.html'
+    model = Post
+# get the objects and arrange in an order
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('date_of_creation')
+
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+# Views for Contacts
+def ContactInfo(request):
+    registered = False
+
+    if request.method == "POST":
+        contactform = ContactForm(data=request.POST)
+
+        if contactform.is_valid():
+            contactform.save()
+
+            registered = True
+            return redirect('thanks')
+        else:
+            print(contactform.errors)
+    else:
+        contactform = ContactForm()
+
+    return render(request,'blog/contact_form.html',{'contactform':contactform})
+
+# comments in post and no login required here
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('thankcomment')
+
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+# only for super user so login login_required
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
